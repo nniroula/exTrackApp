cd5b3b3df84a37f8deaad7235dcebdc90886b3b1
nabin <nkbhaie2017@gmail.com>
2024-04-29 11:15:45 -0600

    add functionalities

--- app/src/main/java/com/noviceModDev/extrackapp/DBHandler.java
+++ app/src/main/java/com/noviceModDev/extrackapp/DBHandler.java
@@ -6,25 +6,30 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHandler extends SQLiteOpenHelper {
-//    private static final String DB_NAME = "exTrack_db"; // use this
-    private static final String DB_NAME = "exT_db"; // use this
+    private static final String DB_NAME = "exTrack_db"; // use this
+//    private static final String DB_NAME = "exT_db"; // use this
     private static final int DB_VERSION = 1;  // use this
 
     // creating a constructor for our database handler.
     public DBHandler(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
     }
-    
+
     @Override
     public void onCreate(SQLiteDatabase db) {
         String houseHoldTable = "CREATE TABLE household(id INTEGER PRIMARY KEY AUTOINCREMENT, item TEXT, date TEXT, cost TEXT)";
+        String usersTable = "CREATE TABLE users(id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)";
+
         db.execSQL(houseHoldTable);
+        db.execSQL(usersTable);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         String tableHouseHold = "DROP TABLE IF EXISTS household";
+        String tableUsers = "DROP TABLE IF EXISTS users";
         db.execSQL(tableHouseHold);
+        db.execSQL(tableUsers);
         onCreate(db);
     }
 
@@ -38,6 +43,19 @@ public class DBHandler extends SQLiteOpenHelper {
 
         db.insert("household", null, values);
 
+        db.close(); // use it at the end only
+
+        return true;
+    }
+
+    public boolean populateUsersTable(String userName, String password){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put("username", userName); // key is column name
+        values.put("password", password);
+
+        db.insert("users", null, values);
         db.close();
 
         return true;
